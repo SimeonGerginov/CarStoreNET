d889d1a5fa9f13b229aa364503162618434032c8
SimeonGerginov <simeongerginov1@gmail.com>
2019-01-25 10:01:46 +0200

    Added ShoppingCartService
    Signed-off-by: SimeonGerginov <simeongerginov1@gmail.com>

---
+++ Source/Services/CarStore.Services/Contracts/IShoppingCartService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+using CarStore.Data.Models;
+
+namespace CarStore.Services.Contracts
+{
+    public interface IShoppingCartService
+    {
+        Task CreateShoppingCart(ClaimsPrincipal currentUser);
+
+        Task AddItemToShoppingCart(int shoppingCartId, int customerId, ShoppingCartItem item);
+
+        Task RemoveItemToShoppingCart(int shoppingCartId, int customerId, ShoppingCartItem item);
+    }
+}
---
+++ Source/Services/CarStore.Services/ShoppingCartService.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using CarStore.Data;
+using CarStore.Data.Models;
+using CarStore.Data.Models.Enums;
+using CarStore.Services.Contracts;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace CarStore.Services
+{
+    public class ShoppingCartService : IShoppingCartService
+    {
+        private readonly UserManager<Customer> _userManager;
+        private readonly CarStoreDbContext _carStoreDbContext;
+
+        public ShoppingCartService(UserManager<Customer> userManager, CarStoreDbContext carStoreDbContext)
+        {
+            this._userManager = userManager;
+            this._carStoreDbContext = carStoreDbContext;
+        }
+
+        public async Task CreateShoppingCart(ClaimsPrincipal currentUser)
+        {
+            var customer = await this._userManager.GetUserAsync(currentUser);
+            var shoppingCart = new ShoppingCart()
+            {
+                Customer = customer,
+                Status = Status.Created
+            };
+
+            await this._carStoreDbContext.ShoppingCarts.AddAsync(shoppingCart);
+            await this._carStoreDbContext.SaveChangesAsync();
+        }
+
+        public async Task AddItemToShoppingCart(int shoppingCartId, int customerId, ShoppingCartItem item)
+        {
+            var shoppingCart =
+                this._carStoreDbContext.ShoppingCarts.FirstOrDefault(sc =>
+                    sc.Id == shoppingCartId && sc.CustomerId == customerId);
+
+            if (shoppingCart == null)
+            {
+                throw new InvalidOperationException("The shopping cart does not exist.");
+            }
+
+            shoppingCart.Status = Status.Incomplete;
+            shoppingCart.ShoppingCartItems.Add(item);
+
+            this._carStoreDbContext.ShoppingCarts.Update(shoppingCart);
+            await this._carStoreDbContext.SaveChangesAsync();
+        }
+
+        public async Task RemoveItemToShoppingCart(int shoppingCartId, int customerId, ShoppingCartItem item)
+        {
+            var shoppingCart =
+                this._carStoreDbContext.ShoppingCarts.FirstOrDefault(sc =>
+                    sc.Id == shoppingCartId && sc.CustomerId == customerId);
+
+            if (shoppingCart == null)
+            {
+                throw new InvalidOperationException("The shopping cart does not exist.");
+            }
+
+            shoppingCart.Status = Status.Incomplete;
+            shoppingCart.ShoppingCartItems.Remove(item);
+
+            this._carStoreDbContext.ShoppingCarts.Update(shoppingCart);
+            await this._carStoreDbContext.SaveChangesAsync();
+        }
+    }
+}
